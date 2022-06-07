a2244d44a64fa622bf29082403ae04cb88591b73
Md.Ashraful Alam <ashrafulalam10000@gmail.com>
2022-06-07 17:00:53 +0600

    age calculator

---
+++ function.js
@@ -0,0 +1,3 @@
+const setAlert = (msg,type='danger')=>{
+  return `<p class="alert alert-${type} d-flex justify-content-between"> ${msg}<button class="btn-close" data-bs-dismiss = "alert"></button> </p>`
+}
